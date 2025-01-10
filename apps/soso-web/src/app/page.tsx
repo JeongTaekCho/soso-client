1df4640cfe731a3dba825dca9b4bc283a358e4f3
JeongTaekCho <cjt3591@gmail.com>
2025-01-10 13:51:26 +0900

    Design: 헤더 컴포넌트

--- apps/soso-web/src/app/page.tsx
+++ apps/soso-web/src/app/page.tsx
@@ -1,3 +1,9 @@
+import Header from '@/shared/components/layout/Header';
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Header type="back" title="헤더" />
+    </div>
+  );
 }
---
+++ apps/soso-web/src/shared/components/layout/Header/index.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import BackIcon from '@/shared/components/icons/BackIcon.tsx';
+import XIcon from '@/shared/components/icons/XIcon';
+import Flex from '@/shared/components/layout/Flex';
+import useBack from '@/shared/hooks/useBack';
+import { ReactNode } from 'react';
+
+interface HeaderProps {
+  type?: 'close' | 'back' | 'customBack' | 'search';
+  title?: string;
+  customBtn?: ReactNode;
+}
+
+export default function Header({ type, title, customBtn }: HeaderProps) {
+  const { handleBack } = useBack();
+
+  return (
+    <div className="h-56 w-full bg-white px-20">
+      {!type && (
+        <Flex align="center" className="h-full w-full">
+          <h1 className="font-title3_bold">{title}</h1>{' '}
+        </Flex>
+      )}
+      {type && type !== 'search' && (
+        <Flex justify="between" align="center" className="h-full w-full">
+          <div>
+            <button>{type === 'close' && <XIcon />}</button>
+            <button onClick={handleBack}>{type === 'back' && <BackIcon />}</button>
+          </div>
+          <div className="font-title4_semi">{title}</div>
+          <div>{customBtn}</div>
+        </Flex>
+      )}
+    </div>
+  );
+}
