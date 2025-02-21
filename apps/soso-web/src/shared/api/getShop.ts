a26470ce5984c0cd6a330c9008f3f997ba4600a8
JeongTaekCho <cjt3591@gmail.com>
2025-02-21 14:58:16 +0900

    Feat: 주변 인기 소품샵 조회 API

--- apps/soso-web/src/shared/api/getShop.ts
+++ apps/soso-web/src/shared/api/getShop.ts
@@ -2,9 +2,9 @@ import { DEFAULT_LOCATION } from '@/shared/constant/location';
 import { ShopType } from '@/shared/types/shopType';
 import { customFetch } from '@/shared/utils/customFetch';
 
-export const getShop = async (lat?: number, lng?: number): Promise<ShopType[]> => {
+export const getShop = async (lat?: number | null, lng?: number | null, sorting?: boolean): Promise<ShopType[]> => {
   const result = await customFetch(
-    `${process.env.NEXT_PUBLIC_SERVER_URL}/shop?lat=${lat || DEFAULT_LOCATION.lat}&lng=${lng || DEFAULT_LOCATION.lng}`
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/shop?lat=${lat || DEFAULT_LOCATION.lat}&lng=${lng || DEFAULT_LOCATION.lng}&sorting=${sorting}`
   );
 
   return result.result;
