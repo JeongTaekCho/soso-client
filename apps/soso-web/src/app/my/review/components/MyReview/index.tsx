7508c003029c13fca7c5bd2400d134704ae33c79
JeongTaekCho <cjt3591@gmail.com>
2025-03-03 14:38:50 +0900

    Design: 마이페이지 내가 등록한 소품샵 UI

--- apps/soso-web/src/app/my/review/components/MyReview/index.tsx
+++ apps/soso-web/src/app/my/review/components/MyReview/index.tsx
@@ -1,17 +1,11 @@
 import Flex from '@/shared/components/layout/Flex';
 import MessageBox from '@/shared/components/layout/Review/components/MessageBox';
-import ProductImage from '@/shared/components/ui/ProductImage';
+import ShopInfo from '@/shared/components/ui/ShopInfo';
 
 export default function MyReview() {
   return (
     <Flex direction="col" gap={20} className="w-full px-16">
-      <Flex align="center" gap={12}>
-        <ProductImage />
-        <Flex direction="col" gap={4}>
-          <h3 className="text-black font-title4_semi">가게이름</h3>
-          <p className="text-gray-400 font-caption">2024.01.01</p>
-        </Flex>
-      </Flex>
+      <ShopInfo name="가게 이름" date="2024.01.01" imgUrl="" />
       <MessageBox>메세지 내용</MessageBox>
     </Flex>
   );
--- apps/soso-web/src/app/my/shop/page.tsx
+++ apps/soso-web/src/app/my/shop/page.tsx
@@ -1,3 +1,38 @@
+'use client';
+
+import Flex from '@/shared/components/layout/Flex';
+import Header from '@/shared/components/layout/Header';
+import ShopInfo from '@/shared/components/ui/ShopInfo';
+import clsx from 'clsx';
+import { useRouter } from 'next/navigation';
+
 export default function MyShopPage() {
-  return <div></div>;
+  const router = useRouter();
+
+  const handleLink = () => {
+    router.push('/shop/20');
+  };
+
+  return (
+    <div>
+      <Header title="내가 등록한 소품샵" type="back" />
+      <Flex direction="col" className="w-full">
+        <button
+          onClick={handleLink}
+          className="flex w-full items-center justify-between border-b border-gray-100 px-16 py-18"
+        >
+          <ShopInfo name="가게 이름" date="2024.01.01" imgUrl="" />
+          <div className={clsx('block w-86 py-6 font-caption', 'rounded-8 bg-gray-50')}>최초 제보</div>
+        </button>
+        <button
+          onClick={handleLink}
+          disabled
+          className="flex w-full items-center justify-between border-b border-gray-100 px-16 py-18"
+        >
+          <ShopInfo name="가게 이름" date="2024.01.01" imgUrl="" disabled />
+          <div className={clsx('block w-86 py-6 font-caption', 'rounded-8 bg-gray-50')}>최초 제보</div>
+        </button>
+      </Flex>
+    </div>
+  );
 }
