5f016f50a25a574e309f93ad63d289e5c9c456c7
JeongTaekCho <cjt3591@gmail.com>
2025-03-04 19:46:42 +0900

    Modify: 소품샵 검색 API 파라미터 명 수정

--- apps/soso-web/src/app/search/components/SearchList/api/getShopSearchList.ts
+++ apps/soso-web/src/app/search/components/SearchList/api/getShopSearchList.ts
@@ -2,6 +2,6 @@ import { SearchListType } from '@/app/search/components/SearchList/types';
 import { customFetch } from '@/shared/utils/customFetch';
 
 export const getShopSearchList = async (shopName: string, page: number, limit: number): Promise<SearchListType> => {
-  const result = await customFetch(`/shop/search?shopName=${shopName}&page=${page}&limit=${limit}`);
+  const result = await customFetch(`/shop/search?keyword=${shopName}&page=${page}&limit=${limit}`);
   return result.result;
 };
