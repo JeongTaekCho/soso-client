0792159a61e648d43da13580b07464f1f77b0ec6
JeongTaekCho <cjt3591@gmail.com>
2025-03-18 13:35:43 +0900

    Feat: 내가 찜한 소품샵 지역별 필터링

--- apps/soso-web/src/app/my/components/ProductLists/api/getMyWish.ts
+++ apps/soso-web/src/app/my/components/ProductLists/api/getMyWish.ts
@@ -1,8 +1,8 @@
 import { GetMyWishResponse } from '@/app/my/components/ProductLists/types';
 import { customFetch } from '@/shared/utils/customFetch';
 
-export const getMyWish = async (page: number, limit: number): Promise<GetMyWishResponse> => {
-  const result = await customFetch(`/user/wishlist?page=${page}&limit=${limit}&area=서울`);
+export const getMyWish = async (page: number, limit: number, area?: string): Promise<GetMyWishResponse> => {
+  const result = await customFetch(`/user/wishlist?page=${page}&limit=${limit}&area=${area || ''}`);
 
   return result.result;
 };
